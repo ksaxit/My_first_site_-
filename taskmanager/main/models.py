538e1ee2d177a31fcf1c2df1a82ddef7affaeeed
mm2231 <saigak0@mail.ru>
2023-01-28 19:27:42 +0300

    first commit

---
+++ taskmanager/main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField('Name', max_length=50)
+    task = models.TextField('Description')
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Target'
+        verbose_name_plural = 'Targets'
